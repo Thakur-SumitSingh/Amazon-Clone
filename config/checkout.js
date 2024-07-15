569355841d24625ad76f11a8802d74a3e64647dc
sumit <sumit123@gmail.com>
2024-07-16 01:14:02 +0530

    fix the radio selector

--- config/checkout.js
+++ config/checkout.js
@@ -49,7 +49,7 @@ cartItemHtml += `
               <div class="delivery-option">
                 <input type="radio" checked
                   class="delivery-option-input"
-                  name="delivery-option-1">
+                  name="delivery-option-${productId}">
                 <div>
                   <div class="delivery-option-date">
                     Tuesday, June 21
@@ -62,7 +62,7 @@ cartItemHtml += `
               <div class="delivery-option">
                 <input type="radio"
                   class="delivery-option-input"
-                  name="delivery-option-1">
+                  name="delivery-option-${productId}">
                 <div>
                   <div class="delivery-option-date">
                     Wednesday, June 15
@@ -75,7 +75,7 @@ cartItemHtml += `
               <div class="delivery-option">
                 <input type="radio"
                   class="delivery-option-input"
-                  name="delivery-option-1">
+                  name="delivery-option-${productId}">
                 <div>
                   <div class="delivery-option-date">
                     Monday, June 13
--- data/cart.js
+++ data/cart.js
@@ -1,6 +1,9 @@
 export const cart = [{
   id: "dd82ca78-a18b-4e2a-9250-31e67412f98d",
   Quantity: 4
+},{
+  id: "3ebe75dc-64d2-4137-8860-1f5a963e534b",
+  Quantity: 4
 }];
 
 export function addToCart(checkingIdd){
